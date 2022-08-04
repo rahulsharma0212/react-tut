dfdba5967866da6c95a9cbc287516a255ad184c2
rahul.sharma <rahul.sharma@mapmyindia.com>
2022-08-04 18:28:00 +0530

    class based approach

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import Person from './Person/Person';
 class App extends Component {
+  state={
+    person:[
+      {name:'Max',age:28},
+      {name:'Manu',age:29},
+      {name:'stephanie',age:26}
+    ]
+  }
+  switchNameHandler=()=>{
+    if(this.state.person[0].name=='Max'){
+    this.setState({
+      person:[
+      {name:'stephanie',age:26},
+      {name:'Manu',age:29},
+      {name:'Max',age:28}]
+    })
+    }else{
+    this.setState({
+      person:[
+      {name:'Max',age:28},
+      {name:'Manu',age:29},
+      {name:'stephanie',age:26}]
+    })
+    }
+
+  }
   render() {
-    const style = {
+   /*  const style = {
       color: "green",
-    };
+    }; */
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
-        <span style={style}>by Rahul</span>
+        {/* <span style={style}>by Rahul</span> */}
+        <p>This is really working</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+        <Person name={this.state.person[1].name}  age={this.state.person[1].age} >My hobbies :: Racing</Person>
+        <Person name={this.state.person[2].name}  age={this.state.person[2].age}/>
       </div>
     );
     // return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Hi I\'m a React App ',React.createElement('span',{style:{color:"green"}},'by Rahul Sharma')));
---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const person = (props)=>{
+        {/* <p>I'm a {props.name} and I am {Math.floor(Math.random()*30)}years old.</p> */}
+    return(
+        <div>
+            <p>I'm a {props.name} and I am {props.age} years old.</p>
+            <p>{props.children}</p>
+        </div>
+    );
+} 
+export default person;
