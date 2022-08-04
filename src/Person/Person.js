f8451e32a89bbecd0b36812e33ea607b0a2fade6
rahul.sharma <rahul.sharma@mapmyindia.com>
2022-08-04 18:52:22 +0530

    functional component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,8 +4,9 @@ const person = (props)=>{
         {/* <p>I'm a {props.name} and I am {Math.floor(Math.random()*30)}years old.</p> */}
     return(
         <div>
-            <p>I'm a {props.name} and I am {props.age} years old.</p>
+            <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old.</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}></input>
         </div>
     );
 } 
