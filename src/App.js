218859a3d9c48e8d93b1af1fae2e38f4e203a8d6
rahul.sharma <rahul.sharma@mapmyindia.com>
2022-08-04 13:50:43 +0530

    jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
+    const style = {
+      color: "green",
+    };
     return (
       <div className="App">
-        <h1>Hi I'm a react App</h1>
+        <h1>Hi I'm a React App</h1>
+        <span style={style}>by Rahul</span>
       </div>
     );
+    // return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Hi I\'m a React App ',React.createElement('span',{style:{color:"green"}},'by Rahul Sharma')));
   }
 }
 
